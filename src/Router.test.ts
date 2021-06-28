b7a32056d47651e213d711321a08f329388896f6
vasiliev-alexey <alex_v81@mail.ru>
2021-06-28 21:58:56 +0300

    refactor

--- src/Router.test.ts
+++
@@ -1,8 +0,0 @@
-// import { Router } from './Router';
-//
-// describe('Router must be class', () => {
-//   it('constructor test', () => {
-//     expect(Router).toBeInstanceOf(Function);
-//     expect(new Router()).toBeInstanceOf(Router);
-//   });
-// });
